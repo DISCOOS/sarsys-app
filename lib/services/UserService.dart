d86fb196717736295710320317d0425b02d1e013
Kenneth Gulbrandsøy <kenneth@discoos.org>
2019-05-05 20:00:49 +0200

    Code formatting

--- lib/services/UserService.dart
+++ lib/services/UserService.dart
@@ -1,12 +1,12 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:jose/jose.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:jose/jose.dart';
 
 class UserService {
   // TODO: Take into account that several users may use the app and handle token storage accordingly
   // TODO: Hash the password and save after successful authentication (for offline login to app)? Possible as long as token is valid?
-
 
   // static final UserService _singleton = new UserService();
   static final storage = new FlutterSecureStorage();
@@ -27,26 +27,26 @@ class UserService {
 
     // TODO: Change to http_client to get better control of timeout, retries etc.
     // TODO: Handle various login/network errors and throw appropriate errors
-      var response = await http.post(
-          url, body: {'username': username, 'password': password});
+    var response = await http
+        .post(url, body: {'username': username, 'password': password});
 
-      // Save to token and other userdata to Secure Storage
-      if (response.statusCode == 200) {
-        // TODO: Validate JWT
-          var responseObject = jsonDecode(response.body);
+    // Save to token and other userdata to Secure Storage
+    if (response.statusCode == 200) {
+      // TODO: Validate JWT
+      var responseObject = jsonDecode(response.body);
 
-          await storage.write(key: 'token', value: responseObject['token']);
-          // TODO: Save other userdata in User object with data from token
-          var jwt = new JsonWebToken.unverified(responseObject['token']);
-          return true;
+      await storage.write(key: 'token', value: responseObject['token']);
+      // TODO: Save other userdata in User object with data from token
+      var jwt = new JsonWebToken.unverified(responseObject['token']);
+      return true;
+    } else if (response.statusCode == 401) {
+      // wrong credentials
+      throw new Exception("Feil brukernavn/passord");
+    } else if (response.statusCode == 403) {
+      // Forbidden
 
-      } else if (response.statusCode == 401) {
-        // wrong credentials
-        throw new Exception("Feil brukernavn/passord");
-      } else if (response.statusCode == 403) {
-        // Forbidden
-        throw new Exception("Du har ikke tilgang til denne applikasjonen");
-      }
+      throw new Exception("Du har ikke tilgang til denne applikasjonen");
+    }
     return false;
   }
 
@@ -62,14 +62,13 @@ class UserService {
           print("token still valid");
           return _tokenFromStorage;
         }
-        } catch (error) {
-          print ("Invalid token structure");
-          return null;
+      } catch (error) {
+        print("Invalid token structure");
+        return null;
       }
     }
     return null;
   }
-
 }
 
 class User {
