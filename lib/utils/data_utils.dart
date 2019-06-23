47208d5ef7ec57fe00d5bc01109ee91ad32220b7
Kenneth Gulbrandsøy <kenneth@discoos.org>
2019-06-23 10:27:37 +0200

    Added parameter for empty position and toLatLng utility method

--- lib/utils/data_utils.dart
+++ lib/utils/data_utils.dart
@@ -1,6 +1,7 @@
 import 'package:SarSys/models/Incident.dart';
 import 'package:SarSys/models/Point.dart';
 import 'package:SarSys/utils/proj4d.dart';
+import 'package:latlong/latlong.dart';
 
 String enumName(Object o) => o.toString().split('.').last;
 String enumDescription(Object o) {
@@ -23,8 +24,8 @@ String toDD(Point point) {
 
 /// TODO: Make UTM zone and northing configurable
 final _utmProj = TransverseMercatorProjection.utm(32, false);
-String toUTM(Point point) {
-  if (point == null) return "Velg";
+String toUTM(Point point, [String empty = "Velg"]) {
+  if (point == null) return empty;
   var src = ProjCoordinate.from2D(point.lon, point.lat);
   var dst = _utmProj.project(src);
   return CoordinateFormat.toUTM(dst);
@@ -34,4 +35,8 @@ String formatSince(DateTime timestamp) {
   if (timestamp == null) return "-";
   Duration delta = DateTime.now().difference(timestamp);
   return delta.inHours > 99 ? "${delta.inDays}d" : delta.inHours > 0 ? "${delta.inHours}h" : "${delta.inSeconds}h";
+}
+
+LatLng toLatLng(Point point) {
+  return LatLng(point.lat, point.lon);
 }
