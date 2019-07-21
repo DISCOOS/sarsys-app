b826da8143250c28a15626693fa3ce42e0a0efcd
Kenneth Gulbrandsøy <kenneth@discoos.org>
2019-07-21 15:41:37 +0200

    Remove unused imports

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:SarSys/blocs/user_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
