0a19c8df0c91ae367d7e004c7309fa8d6a281ebf
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-08-31 15:46:49 +0200

    Left-adjustment of coordinates in panel

--- lib/map/layers/coordate_layer.dart
+++ lib/map/layers/coordate_layer.dart
@@ -40,17 +40,18 @@ class CoordinateLayer extends MapPlugin {
               Align(
                 alignment: params.align,
                 child: Container(
-                  margin: EdgeInsets.all(16.0),
+                  margin: EdgeInsets.all(8.0),
                   padding: EdgeInsets.all(16.0),
-                  height: 72.0,
+                  height: 80.0,
                   decoration: BoxDecoration(
                     color: Colors.white.withOpacity(0.8),
                     borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      if (center != null) Text(toUTM(center)),
-                      if (center != null) Text(toDD(center)),
+                      if (center != null) Text(toUTM(center), textAlign: TextAlign.start),
+                      if (center != null) Text(toDD(center), textAlign: TextAlign.start),
                     ],
                   ),
                 ),
