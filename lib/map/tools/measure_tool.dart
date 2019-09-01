6076d3a9d75505e09ad28bcb9c5d373f17e1f8b4
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-09-02 00:28:50 +0200

    Fixed NPE when deleting measurement on empty list

--- lib/map/tools/measure_tool.dart
+++ lib/map/tools/measure_tool.dart
@@ -10,7 +10,7 @@ class MeasureTool extends MapTool with MapEditable<Set<LatLng>> {
 
   void init() => changed(() => target..clear());
   void add(LatLng point) => changed(() => target.add(point));
-  void remove() => changed(() => target.remove(target.last));
+  void remove() => changed(() => {if (target.isNotEmpty) target.remove(target.last)});
 
   @override
   bool onTap(BuildContext context, LatLng point, double tolerance, onMatch) {
