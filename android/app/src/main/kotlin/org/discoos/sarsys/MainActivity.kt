0c4d154a1b349125424403d5405dfbe23feed255
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-10-29 20:55:35 +0100

    Turn of secure screen setting for easier debugging and testing

--- android/app/src/main/kotlin/org/discoos/sarsys/MainActivity.kt
+++ android/app/src/main/kotlin/org/discoos/sarsys/MainActivity.kt
@@ -9,7 +9,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+//    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     GeneratedPluginRegistrant.registerWith(this)
   }
 }
