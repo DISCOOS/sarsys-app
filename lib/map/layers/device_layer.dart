5fce45d0fa71a38ae8ac819073817eb950ff88ed
Kenneth Gulbrandsoy <kenneth@discoos.org>
2019-10-29 21:30:07 +0100

    Added NPE checks for empty tracks list, close #36.

--- lib/map/layers/device_layer.dart
+++ lib/map/layers/device_layer.dart
@@ -84,7 +84,7 @@ class DeviceLayer extends MapPlugin {
 
   List<Point> _toTrack(Map<String, Set<Tracking>> tracking, Device device) {
     final tracks = tracking[device.id]?.first?.tracks;
-    return tracks == null ? [] : tracks[device.id].points;
+    return tracks[device.id]?.points ?? [];
   }
 
   _buildTrack(
