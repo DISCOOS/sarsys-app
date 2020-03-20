e5afdcfbbac2bdb356415479ce02c4a6667cc67e
Kenneth Gulbrandsoy <kenneth@discoos.org>
2020-03-20 19:45:17 +0100

    Migrated Android files after upgrade to flutter 1.12

--- android/app/src/main/kotlin/org/discoos/sarsys/MainActivity.kt
+++ android/app/src/main/kotlin/org/discoos/sarsys/MainActivity.kt
@@ -1,15 +1,12 @@
 package org.discoos.sarsys
 
-import android.os.Bundle
-import android.view.*;
-
-import io.flutter.app.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-//    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-    GeneratedPluginRegistrant.registerWith(this)
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
 }
