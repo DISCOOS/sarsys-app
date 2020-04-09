805ce4107e38489bb4f3735af048a4499234e072
Kenneth Gulbrandsoy <kenneth@discoos.org>
2020-04-09 18:54:51 +0200

    Added Gravatar support to app drawer Added roles output to app drawer Added security mode output to app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,9 +1,12 @@
+import 'package:SarSys/blocs/app_config_bloc.dart';
 import 'package:SarSys/blocs/incident_bloc.dart';
 import 'package:SarSys/blocs/user_bloc.dart';
+import 'package:SarSys/models/Security.dart';
 import 'package:SarSys/models/User.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:simple_gravatar/simple_gravatar.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -11,26 +14,68 @@ class AppDrawer extends StatelessWidget {
     final UserBloc userBloc = BlocProvider.of<UserBloc>(context);
     final User user = userBloc.user;
     final isUnset = BlocProvider.of<IncidentBloc>(context).isUnset;
-    final avatar = Image.asset("assets/images/avatar-male.png");
+    var gravatar = Gravatar(user.email);
+    var url = gravatar.imageUrl(
+      size: 100,
+      defaultImage: GravatarImage.mp,
+      fileExtension: true,
+    );
+    final avatar = Image.network(url);
+    final roles = user.roles.toList();
+    roles.sort((UserRole e1, UserRole e2) => e1.index - e2.index);
     return Drawer(
       child: ListView(
         // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text(
-              "${user?.fullName}",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            accountEmail: Text(
-              user.email,
-              style: TextStyle(fontWeight: FontWeight.w400),
-            ),
-            currentAccountPicture: CircleAvatar(
-              radius: 24,
-              backgroundColor: Theme.of(context).accentColor,
-              child: avatar,
-            ),
+          Stack(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text(
+                  "${user?.fullName}",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                accountEmail: Text(
+                  user.email,
+                  style: TextStyle(fontWeight: FontWeight.w400),
+                ),
+                currentAccountPicture: CircleAvatar(
+                  radius: 24,
+                  backgroundImage: avatar.image,
+                  backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+                ),
+              ),
+              SafeArea(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0, right: 16.0),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.45,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Chip(
+                            padding: EdgeInsets.zero,
+                            label: Text(
+                              '${translateSecurityMode(BlocProvider.of<AppConfigBloc>(context).config.securityMode)} app',
+                              style: TextStyle(color: Colors.white38),
+                            ),
+                            backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+                          ),
+                          Text(
+                            roles.isEmpty ? 'Ingen roller' : roles.map(translateUserRole).join(', '),
+                            style: TextStyle(color: Colors.white38),
+                            textAlign: TextAlign.end,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
           ListTile(
             leading: const Icon(Icons.format_list_bulleted),
