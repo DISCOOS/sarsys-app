1693a94be98fc98918d1f2caf2957f83ef188379
Kenneth Gulbrandsoy <kenneth@discoos.org>
2020-05-18 01:03:11 +0200

    Fixed bug in affiliation form

--- lib/widgets/affilliation.dart
+++ lib/widgets/affilliation.dart
@@ -61,7 +61,7 @@ class AffiliationView extends StatelessWidget {
               child: buildCopyableText(
                 context: context,
                 label: "Tilhørighet",
-                icon: SarSysIcons.of(snapshot.data?.id),
+                icon: SarSysIcons.of(affiliation.orgId),
                 value: snapshot.hasData ? snapshot.data.toFullName(affiliation) : '-',
                 onMessage: onMessage,
                 onComplete: onComplete,
@@ -95,40 +95,53 @@ class AffiliationFormState extends State<AffiliationForm> {
 
   final _formKey = GlobalKey<FormBuilderState>();
 
-  ValueNotifier<Organization> _organization = ValueNotifier(null);
+  Future<Organization> _future;
+  ValueNotifier<Organization> _org = ValueNotifier(null);
   ValueNotifier<Division> _division = ValueNotifier(null);
 
-  String _department;
+  String _depId;
 
   @override
   void initState() {
     super.initState();
-    _department = widget.initialValue.depId;
-    FleetMapService().fetchOrganization(Defaults.orgId)..then(_resolve);
+    _depId = widget.initialValue.depId;
+  }
+
+  @override
+  void didChangeDependencies() {
+    _future = FleetMapService().fetchOrganization(Defaults.orgId)..then(_resolve);
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(AffiliationForm oldWidget) {
+    if (oldWidget.user != widget.user) {
+      _future = FleetMapService().fetchOrganization(Defaults.orgId)..then(_resolve);
+    }
+    super.didUpdateWidget(oldWidget);
   }
 
   void _resolve(Organization org) {
+    var division = org.divisions[widget.initialValue.divId];
     if (widget.user != null) {
       if (widget.user.division != null) {
-        final division = org.divisions.values.firstWhere(
+        division = org.divisions.values.firstWhere(
           (match) => match.name == widget.user.division,
           orElse: () => null,
         );
         if (division != null) {
           if (widget.user.department != null) {
-            _department = division.departments.values.firstWhere(
+            _depId = division.departments.values.firstWhere(
               (match) => match == widget.user.department,
               orElse: () => null,
             );
           }
-          _organization.value = org;
-          _division.value = division;
         }
       }
-    } else {
-      _organization.value = org;
-      _division.value = org.divisions[widget.initialValue.divId];
     }
+    _org.value = org;
+    _formKey.currentState.value['org'] = org.id;
+    _division.value = division;
   }
 
   @override
@@ -150,20 +163,95 @@ class AffiliationFormState extends State<AffiliationForm> {
   }
 
   Widget _buildOrganizationField() {
-    return ValueListenableBuilder<Organization>(
-        valueListenable: _organization,
-        builder: (context, org, _) {
-          _update('organization', Defaults.orgId);
+    return FutureBuilder<Organization>(
+        future: _future,
+        builder: (context, snapshot) {
+          final org = snapshot.hasData ? snapshot.data : null;
+          _update(ORG_FIELD, org?.id);
           return _buildReadOnly(
             context,
-            'organization',
+            ORG_FIELD,
             'Organisasjon',
-            org?.name ?? "-",
+            org?.name,
+            org?.id,
           );
         });
   }
 
-  FormBuilderCustomField<String> _buildReadOnly(BuildContext context, String attribute, String title, String value) {
+  static const ORG_FIELD = 'orgId';
+  static const DIV_FIELD = 'divId';
+  static const DEP_FIELD = 'depId';
+
+  Widget _buildDivisionField() {
+    return ValueListenableBuilder<Organization>(
+      valueListenable: _org,
+      builder: (context, org, _) {
+        final divId = _update(DIV_FIELD, _ensureDivId(org));
+        final division = org?.divisions?.elementAt(divId);
+        return org != null && editable
+            ? buildDropDownField<String>(
+                attribute: DIV_FIELD,
+                label: 'Distrikt',
+                initialValue: divId,
+                items: _ensureDivisions(org),
+                onChanged: (selected) {
+                  if (org != null) {
+                    _division.value = org.divisions[selected];
+                    _onChanged();
+                  }
+                },
+                validators: [
+                  FormBuilderValidators.required(errorText: 'Distrikt må velges'),
+                ],
+              )
+            : _buildReadOnly(
+                context,
+                DIV_FIELD,
+                'Distrikt',
+                division?.name,
+                divId,
+              );
+      },
+    );
+  }
+
+  Widget _buildDepartmentField() {
+    return ValueListenableBuilder<Division>(
+        valueListenable: _division,
+        builder: (context, division, _) {
+          final depId = _update(DEP_FIELD, _ensureDepId(division));
+          return editable
+              ? buildDropDownField<String>(
+                  attribute: DEP_FIELD,
+                  label: 'Avdeling',
+                  items: _ensureDepartments(division),
+                  initialValue: _depId,
+                  enabled: editable,
+                  onChanged: (selected) {
+                    _depId = selected;
+                    _onChanged();
+                  },
+                  validators: [
+                    FormBuilderValidators.required(errorText: 'Avdeling må velges'),
+                  ],
+                )
+              : _buildReadOnly(
+                  context,
+                  DEP_FIELD,
+                  'Avdeling',
+                  division?.departments[depId],
+                  depId,
+                );
+        });
+  }
+
+  FormBuilderCustomField<String> _buildReadOnly(
+    BuildContext context,
+    String attribute,
+    String label,
+    String title,
+    String value,
+  ) {
     return FormBuilderCustomField<String>(
       attribute: attribute,
       formField: FormField<String>(
@@ -178,7 +266,7 @@ class AffiliationFormState extends State<AffiliationForm> {
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 4.0),
             child: Text(
-              value,
+              title ?? '-',
               style: Theme.of(context).textTheme.subtitle2,
             ),
           ),
@@ -187,46 +275,23 @@ class AffiliationFormState extends State<AffiliationForm> {
     );
   }
 
-  Widget _buildDivisionField() {
-    return ValueListenableBuilder<Organization>(
-      valueListenable: _organization,
-      builder: (context, org, _) {
-        final division = _update('division', _ensureDivision(org));
-        return widget.user == null
-            ? buildDropDownField<String>(
-                attribute: 'division',
-                label: 'Distrikt',
-                initialValue: division,
-                items: _ensureDivisions(org),
-                onChanged: (value) {
-                  if (org != null) {
-                    _division.value = org.divisions[value];
-                    _onChanged();
-                  }
-                },
-                validators: [
-                  FormBuilderValidators.required(errorText: 'Distrikt må velges'),
-                ],
-              )
-            : _buildReadOnly(
-                context,
-                'division',
-                'Distrikt',
-                _division?.value?.name ?? '-',
-              );
-      },
-    );
-  }
+  bool get editable => widget.user == null;
 
   String _update(String attribute, String value) {
     _formKey.currentState.value[attribute] = value;
-    Future.microtask(() => _formKey.currentState.fields[attribute].currentState.didChange(value));
+    Future.microtask(() => _formKey.currentState.fields[attribute]?.currentState?.didChange(value));
     return value;
   }
 
-  String _ensureDivision(Organization org) => org?.divisions?.containsKey(widget.initialValue?.divId) == true
-      ? widget.initialValue?.divId
-      : org?.divisions?.keys?.first ?? Defaults.divId;
+  String _ensureDivId(Organization org) {
+    final divId = _formKey.currentState.value[DIV_FIELD] ?? widget.initialValue?.divId ?? Defaults.depId;
+    return org?.divisions?.containsKey(divId) == true ? divId : org?.divisions?.keys?.first;
+  }
+
+  String _ensureDepId(Division division) {
+    final depId = _depId ?? widget.initialValue?.depId ?? Defaults.depId;
+    return (division?.departments?.containsKey(depId) == true ? depId : division?.departments?.keys?.first);
+  }
 
   List<DropdownMenuItem<String>> _ensureDivisions(Organization org) {
     return sortMapValues<String, Division, String>(org?.divisions ?? {}, (division) => division.name)
@@ -236,40 +301,6 @@ class AffiliationFormState extends State<AffiliationForm> {
               child: Text("${division.value.name}"),
             ))
         .toList();
-  }
-
-  Widget _buildDepartmentField() {
-    return ValueListenableBuilder<Division>(
-        valueListenable: _division,
-        builder: (context, division, _) {
-          final department = _ensureDepartment(division);
-          _update('department', department);
-          return widget.user == null
-              ? buildDropDownField<String>(
-                  attribute: 'department',
-                  label: 'Avdeling',
-                  items: _ensureDepartments(division),
-                  initialValue: null,
-                  enabled: widget.user == null,
-                  onChanged: (_) => _onChanged(),
-                  validators: [
-                    FormBuilderValidators.required(errorText: 'Avdeling må velges'),
-                  ],
-                )
-              : _buildReadOnly(
-                  context,
-                  'department',
-                  'Avdeling',
-                  _division?.value?.departments?.elementAt(department) ?? '-',
-                );
-        });
-  }
-
-  String _ensureDepartment(Division division) {
-    final department = _department ?? widget.initialValue?.depId;
-    return (division?.departments?.containsKey(department) == true
-        ? department
-        : division?.departments?.keys?.first ?? Defaults.depId);
   }
 
   List<DropdownMenuItem<String>> _ensureDepartments(Division division) {
