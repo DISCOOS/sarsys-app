924fa0dd465d5d4ea32c557eb094a24eb0cd6e8b
Anders Scisly Sagevik <anders@sagevik.no>
2020-12-13 00:27:35 +0100

    Removing typo

--- lib/features/operation/domain/usecases/operation_use_cases.dart
+++ lib/features/operation/domain/usecases/operation_use_cases.dart
@@ -169,7 +169,7 @@ class LeaveOperation extends UseCase<bool, Personnel, OperationParams> {
     assert(params.data == null, "Operation should not be given");
 
     final leave = await prompt(
-      params.overlay.context,q
+      params.overlay.context,
       'Bekreftelse',
       'Du dimitteres nå fra aksjonen. Vil du fortsette?',
     );
